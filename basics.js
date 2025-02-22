a77329d6b8b0bfba01de8faf3c42e9f4f66d2c40
Muhammad Talha Iftikhar <talhaiftikhar13209@gmail.com>
2025-02-22 20:12:26 +0500

    const,let,var

--- basics.js
+++ basics.js
@@ -1,1 +1,7 @@
 console.log("hello world");
+
+const a = 10;   // this "a" con't be changed it's a constant
+
+let b = 20;    // this variable can be changed it's a variable
+
+var c = 30;    // this variable can be changed it's a variable but nowadays no one use this
