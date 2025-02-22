f8c161e887639ea1b72753ea3ceb86a0ff9f19f1
Muhammad Talha Iftikhar <talhaiftikhar13209@gmail.com>
2025-02-22 20:16:04 +0500

    data types

--- basics.js
+++ basics.js
@@ -1,7 +1,26 @@
-console.log("hello world");
+// console.log("hello world");
 
-const a = 10;   // this "a" con't be changed it's a constant
+// const a = 10;   // this "a" con't be changed it's a constant
 
-let b = 20;    // this variable can be changed it's a variable
+// let b = 20;    // this variable can be changed it's a variable
 
-var c = 30;    // this variable can be changed it's a variable but nowadays no one use this
+// var c = 30;    // this variable can be changed it's a variable but nowadays no one use this
+
+let s = "Talha";
+let b = true;
+let n = null;
+let u = undefined;
+let o = NaN;
+let i = Infinity;
+let f = 3.14;
+let a = 33;
+
+console.log(typeof a);
+console.log(typeof f);
+console.log(typeof i);
+console.log(typeof u);
+console.log(typeof n);
+console.log(typeof o);
+
+console.log(typeof s);
+console.log(typeof b);
