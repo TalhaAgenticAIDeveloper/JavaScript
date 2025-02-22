fc9a2f081ab4999fb4dcb3b02bdc06d17ce5e6dc
Muhammad Talha Iftikhar <talhaiftikhar13209@gmail.com>
2025-02-22 21:37:31 +0500

    data types

---
+++ dataTypes.js
@@ -0,0 +1,14 @@
+const id = Symbol("123");
+const anotherId = Symbol("123");
+
+
+
+console.log(id === anotherId);
+
+console.log(id == anotherId);
+
+console.log(id);
+console.log(anotherId);
+
+
+
