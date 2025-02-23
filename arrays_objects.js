4bcbce94465f45e7b4d6678d7376f36cd29b9d12
Muhammad Talha Iftikhar <talhaiftikhar13209@gmail.com>
2025-02-23 21:31:23 +0500

    object

--- arrays_objects.js
+++ arrays_objects.js
@@ -5,3 +5,18 @@ arr1 = [1,2,3,4,5,6,7,8,9]
 console.log(arr1[0]);
 console.log(arr1[1]);
 console.log(arr1[2]);
+
+
+
+
+let obj1 = {
+    name: "Talha",
+    age: 22,
+    gender: "male"
+}
+
+
+console.log(obj1["name"]);
+console.log(obj1.name);
+
+
