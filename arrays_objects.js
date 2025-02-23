4eb319a77923143623183607ae7dae4342ed0423
Muhammad Talha Iftikhar <talhaiftikhar13209@gmail.com>
2025-02-23 21:34:38 +0500

    symbol in object done

--- arrays_objects.js
+++ arrays_objects.js
@@ -7,16 +7,20 @@ console.log(arr1[1]);
 console.log(arr1[2]);
 
 
-
+const key1 = Symbol("key1");
 
 let obj1 = {
     name: "Talha",
+    [key1] :Symbol("key1"),
     age: 22,
     gender: "male"
 }
 
 
-console.log(obj1["name"]);
-console.log(obj1.name);
+// console.log(obj1["name"]);
+// console.log(obj1.name);
+
+console.log(typeof obj1[key1]);
+
 
 
