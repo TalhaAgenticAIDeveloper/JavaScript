2ce4f197a8b3dc9a7164390f48af0ea80b0bc10e
Muhammad Talha Iftikhar <talhaiftikhar13209@gmail.com>
2025-02-25 17:54:29 +0500

    basic syntax of functions

---
+++ functions.js
@@ -0,0 +1,26 @@
+function one(){
+    const name = "Talha";
+    function two(){
+        last_name = "Iftikhar";
+        // console.log(name);
+    }
+    // console.log(last_name);
+    
+    two();
+}
+
+
+one();
+
+
+
+const func = function add(num){
+
+    return num + 1;
+}
+
+
+// add(5)
+
+console.log(func(5));
+
