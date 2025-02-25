5c28b2e9f99c18738fb14c5a5913cbd86c403a93
Muhammad Talha Iftikhar <talhaiftikhar13209@gmail.com>
2025-02-25 18:22:38 +0500

    allow functions done

--- functions.js
+++ functions.js
@@ -1,4 +1,4 @@
-one();
+// one();
 function one(){
     const name = "Talha";
     function two(){
@@ -15,7 +15,7 @@ function one(){
 
 
 // func(5);     not allowed(hositing)
-const func = function add(num){
+const func1 = function(num){
 
     return num + 1;
 }
@@ -23,5 +23,110 @@ const func = function add(num){
 
 // add(5)       now allowed
 
-console.log(func(5));   // allowed
+// console.log(func(5));   // allowed
 
+
+
+
+
+
+
+
+
+
+
+// +++++++++++++++++++++++++++++++ This ++++++++++++++++++++++++++++++++++++ //
+
+
+
+// function ok(){
+
+//     console.log(this);
+    
+// }
+
+// ok();
+
+
+
+
+
+
+
+
+
+
+
+
+
+// this dosn't work in functions
+// function ok(){  
+//     let username = "Talha";
+
+//     console.log(this.username);
+    
+
+// }
+
+// ok();
+
+// console.log(username);
+
+
+
+
+
+
+
+// this dosn't work in Arrow functions
+// const func2 = () => {
+//     hello = "hello";
+//     console.log(this);
+// }
+
+// func2();
+
+
+
+
+
+
+
+
+// in arrow functions with '{}' return statement is mandatory
+// const add = (num1,num2) => {return num1 + num2}
+
+// console.log(add(4,5));
+
+
+
+
+
+
+
+
+// in arrow functions with '()' return statement is not required
+// const add = (num1,num2) => ( num1 + num2)
+
+// console.log(add(4,5));
+
+
+
+
+
+
+
+
+// const add = (num1,num2) => num1 + num2
+
+// console.log(add(4,5));
+
+
+
+// To return object in arrow function we need to inclose that object in '()'
+// const obj = () => ({usrname: "Talha",
+//                             age: 22,
+//                             fee: 20000
+// })
+
+// console.log(obj());
